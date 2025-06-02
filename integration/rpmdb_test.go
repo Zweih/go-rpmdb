e78554c3cb7d5e42d3eed2024f559a3b34d1e1d1
Fernando Nunez <15152269+Zweih@users.noreply.github.com>
2025-06-02 14:13:22 -0600

    Fix: Add sqlite to Windows runner (#10)

--- integration/rpmdb_test.go
+++ integration/rpmdb_test.go
@@ -10,7 +10,6 @@ import (
 	"github.com/Zweih/go-rpmdb/pkg"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	// _ "github.com/glebarez/go-sqlite"
 )
 
 var packageTests = []struct {
